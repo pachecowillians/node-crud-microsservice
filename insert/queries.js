cf07ecdde2bb8d1734cf45289da1eb1e953a76bb
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-05-03 21:16:48 -0300

    Removing unnecessary code

--- insert/queries.js
+++ insert/queries.js
@@ -7,26 +7,6 @@ const pool = new Pool({
     port: 5432,
 })
 
-const getGeeks = (request, response) => {
-    pool.query('SELECT * FROM geeks ORDER BY id asc', (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-
-const getGeekById = (request, response) => {
-    const id = parseInt(request.params.id)
-
-    pool.query('SELECT * FROM geeks WHERE id = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).json(results.rows)
-    })
-}
-
 const createGeek = (request, response) => {
     const { name } = request.body
 
@@ -38,37 +18,6 @@ const createGeek = (request, response) => {
     })
 }
 
-const updateGeek = (request, response) => {
-    const id = parseInt(request.params.id)
-    const { name } = request.body
-
-    pool.query(
-        'UPDATE geeks SET name = $1 WHERE id = $2',
-        [name, id],
-        (error, results) => {
-            if (error) {
-                throw error
-            }
-            response.status(200).send(`Geek modified with ID: ${id}`)
-        }
-    )
-}
-
-const deleteGeek = (request, response) => {
-    const id = parseInt(request.params.id)
-
-    pool.query('DELETE FROM geeks WHERE id = $1', [id], (error, results) => {
-        if (error) {
-            throw error
-        }
-        response.status(200).send(`Geek deleted with ID: ${id}`)
-    })
-}
-
 module.exports = {
-    getGeeks,
-    getGeekById,
     createGeek,
-    updateGeek,
-    deleteGeek,
 }
