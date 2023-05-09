5460befbb67a2e79724d788f16d0d946086ed6ca
Willian Pacheco Silva <willianpacheco31@gmail.com>
2023-05-09 11:32:04 -0300

    Updating container settings

--- delete/queries.js
+++ delete/queries.js
@@ -1,8 +1,13 @@
 const Pool = require('pg').Pool
+
+require('dotenv').config()
+
+const host = process.env.HOST;
+
 const pool = new Pool({
     user: 'username',
-    host: '172.17.0.1',
-    database: 'default_database',
+    host: host,
+    database: 'db_api',
     password: 'password',
     port: 5432,
 })
